81fe8259fe04989b9bd83b55ab9dcf8e31594009
unknown <dilip_d_karki@yahoo.com>
2020-09-19 23:33:04 -0700

    resetting db to local

--- src/WallStreet/settings.py
+++ src/WallStreet/settings.py
@@ -97,38 +97,38 @@ WSGI_APPLICATION = 'WallStreet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'wallstreet',
-#        'USER': 'postgres',
-#        'PASSWORD': 'admin',
-#        'HOST': 'localhost'
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wallstreet',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
+    }
+}
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ebdb',
-            'USER': 'postgres',
-            'PASSWORD': 'Champ2424',
-            'HOST': 'aa1rgau2058nibn.ciqodeqxwwle.us-west-1.rds.amazonaws.com',
-            'PORT':'5432'
-        }
-    }
+#if 'RDS_DB_NAME' in os.environ:
+#    DATABASES = {
+#        'default': {
+#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': os.environ['RDS_DB_NAME'],
+#            'USER': os.environ['RDS_USERNAME'],
+#            'PASSWORD': os.environ['RDS_PASSWORD'],
+#            'HOST': os.environ['RDS_HOSTNAME'],
+#            'PORT': os.environ['RDS_PORT'],
+#        }
+#    }
+#else:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': 'ebdb',
+#            'USER': 'postgres',
+#            'PASSWORD': 'Champ2424',
+#            'HOST': 'aa1rgau2058nibn.ciqodeqxwwle.us-west-1.rds.amazonaws.com',
+#            'PORT':'5432'
+#        }
+#    }
 
 
 # Password validation
